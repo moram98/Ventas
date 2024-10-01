3c87e6951ea51898a0ad56cda60047c0683a8817
moram98 <moram7992@gmail.com>
2024-09-30 19:57:52 -0500

    Actualizacion

--- layout/parte1.php
+++ layout/parte1.php
@@ -58,7 +58,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="<?php echo $URL; ?>/public/template/AdminLTE-3.2.0/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Ventas Online</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
@@ -128,12 +128,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <a href="<?php echo $URL; ?> /usuarios/index.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Listado de categorias </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?php echo $URL; ?> /usuarios/creacion.php" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Creacion de usuario</p>
                 </a>
               </li>
             </ul>
